95a2ddd6a52132101ef2fdc17e8cca9c391fcbe1
Wim Vander Schelden <lists@fixnum.org>
2016-08-09 20:53:13 +0200

    Attempt #2: plugin loads and executes OK, then SIGSEGV.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,27 @@ use libloading::{Symbol, Library};
 extern crate shared_traits;
 use shared_traits::SharedTrait;
 
-fn load_plugin() -> libloading::Result<Box<SharedTrait>> {
+struct Plugin {
+    lib: Library,
+    instance: Box<SharedTrait>,
+}
+
+fn load_plugin() -> libloading::Result<Plugin> {
     let lib = try!(Library::new("./libdynamic_plugin.so"));
+    let mut instance: Option<Box<SharedTrait>> = None;
     unsafe {
-        let func: Symbol<unsafe extern fn() -> Box<SharedTrait>> = try!(lib.get(b"create_something\0"));
-        println!("Function found.");
-        Ok(func())
+        let func: Symbol<unsafe extern fn() -> Box<SharedTrait>> = lib.get(b"create_something\0").unwrap();
+        instance = Some(func());
     }
+    Ok(Plugin {
+        lib: lib,
+        instance: instance.unwrap(),
+    })
 }
 
 fn main() {
     match load_plugin() {
-        Ok(plugin) => println!("Plugin loaded, test result: {}.", plugin.test_function()),
+        Ok(plugin) => println!("Plugin loaded, test result: {}.", plugin.instance.test_function()),
         Err(e) => println!("Failed to load plugin: {}.", e),
     }
 }
